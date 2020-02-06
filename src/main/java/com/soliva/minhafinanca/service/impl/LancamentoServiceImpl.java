262cc30d14d2293b559e068757d6423de529ada2
Michel Soliva <michelsolivadev@gmail.com>
2020-02-06 14:43:35 -0300

    Criando o Serviço de Lancamento

---
+++ src/main/java/com/soliva/minhafinanca/service/impl/LancamentoServiceImpl.java
@@ -0,0 +1,52 @@
+package com.soliva.minhafinanca.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.soliva.minhafinanca.model.entity.Lancamento;
+import com.soliva.minhafinanca.model.enums.StatusLancamento;
+import com.soliva.minhafinanca.model.repository.LancamentoRepository;
+import com.soliva.minhafinanca.service.LancamentoService;
+
+@Service
+public class LancamentoServiceImpl implements LancamentoService {
+	
+	private LancamentoRepository repository;
+
+	public LancamentoServiceImpl(LancamentoRepository repository) {
+		this.repository = repository;
+	}
+	
+	@Override
+	public Lancamento salvar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Lancamento atualizar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
